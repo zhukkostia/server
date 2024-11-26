912ca3e7674c2ab3bec93d3518d15d0a38eea673
zhukkostia <158212889+zhukkostia@users.noreply.github.com>
2024-11-26 17:20:54 +0200

    Update server.py

--- server.py
+++ server.py
@@ -11,9 +11,9 @@ def read_root():
 
 def start_tcp_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(("0.0.0.0", 10000))  
+    server_socket.bind(("0.0.0.0", 8000))  
     server_socket.listen(5)
-    print("TCP-сервер запущено на 127.0.0.1:65432")
+    print("TCP-сервер запущено)
 
     while True:
         client_socket, client_address = server_socket.accept()
